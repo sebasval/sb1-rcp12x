4cd2eedcdb8824849fc822ebc1f3ec13ca2b1aed
sebasval <s.dvh27@gmail.com>
2024-11-17 14:31:15 -0500

    fix imports

--- src/components/ResultsChart.tsx
+++ src/components/ResultsChart.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useRef } from 'react';
 import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from 'chart.js';
 import { Bar } from 'react-chartjs-2';
 
